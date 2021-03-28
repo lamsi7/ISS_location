f12ebae43898e8aaab9f3938b95079c95425ee90
Dominika Klepacka <klepacka.dominika95@gmail.com>
2021-03-28 13:50:46 +0200

    feat: get astronauts names that are on ISS

--- coor.py
+++ coor.py
@@ -13,3 +13,14 @@ def iss_req():
 
     else:
         return "Error:", r.status_code
+
+
+def astro_names():
+    """Get current names of astronauts on ISS """
+    url = 'http://api.open-notify.org/astros.json'
+    r = requests.get(url)
+    if r.status_code == 200:
+        r_dict = r.json()
+        # Get names on ISS:
+        people = [x['name'] for x in r_dict['people'] if x['craft'] == 'ISS']
+    return people
