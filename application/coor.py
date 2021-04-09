a879802c337a47218bec9c39d04f4e3979c7b412
Dominika Klepacka <klepacka.dominika95@gmail.com>
2021-04-09 21:22:07 +0200

    feat: coor moved to application folder to keep all together

---
+++ application/coor.py
@@ -0,0 +1,26 @@
+import requests
+
+
+def iss_req():
+    """Get current location of ISS"""
+    url = 'http://api.open-notify.org/iss-now.json'
+    r = requests.get(url)
+    if r.status_code == 200:
+        r_dict = r.json()
+        current_position = r_dict['iss_position']
+        print('Current position of ISS:', current_position)
+        return current_position
+
+    else:
+        return "Error:", r.status_code
+
+
+def astro_names():
+    """Get current names of astronauts on ISS """
+    url = 'http://api.open-notify.org/astros.json'
+    r = requests.get(url)
+    if r.status_code == 200:
+        r_dict = r.json()
+        # Get names on ISS:
+        people = [x['name'] for x in r_dict['people'] if x['craft'] == 'ISS']
+    return people
