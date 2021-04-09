1c7d517e213a90b3136dc936ee895c7a522666b2
Dominika Klepacka <klepacka.dominika95@gmail.com>
2021-04-09 21:21:29 +0200

    feat: init file

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from .plotlydash.dashboard import init_dashboard
 
 
 def init_app():
+    """Construct core Flask application with embedded Dash app."""
     app = Flask(__name__)
+    with app.app_context():
+        # Import parts of our core Flask app
+        from . import routes
+
+        # Import Dash application
+        from .plotlydash.dashboard import init_dashboard
+        app = init_dashboard(app)
     return app
