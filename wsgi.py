2b1f0c52b7e3e06a00caf0a3964b62c2c90bda7e
Dominika Klepacka <klepacka.dominika95@gmail.com>
2021-05-02 00:54:29 +0200

    feat: check if grd file exists. if no, download it

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,16 @@
 from application import init_app
+from download_grd import *
 
+grd_file = 'ETOPO1_Ice_g_gdal.grd.gz'
+#Check if ETOPO1_Ice_g_gdal.grd.gz is downloaded:
+if not os.path.isfile(grd_file):
+    url = f"https://www.ngdc.noaa.gov/mgg/global/relief/ETOPO1/data/ice_surface/grid_registered/netcdf/{grd_file}"
+    print(f"Downloading {grd_file} (377Mb)...")
+    download_file(url)
+    print("Extraction...")
+    extract_gz(grd_file)
+    #Leave only .grd file
+    os.remove(grd_file)
 
 app = init_app()
 
